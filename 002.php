fe97f8245a07346c6bf1c8de850ec14c0e569920
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-10 10:43:58 +0200

    final

--- 002.php
+++ 002.php
@@ -1,7 +1,14 @@
-$birthYear = 2003;
-$currentYear =2025;
+<?php
 
-if ($birthYear > $currentYear ){
+$birthYear = 2027;
+$currentYear = 2025;
+$age = $currentYear - $birthYear;
 
 
+if ($birthYear > $currentYear) {
+    echo 'Il numero da te inserito non può essere elaborato';
+} else if ($age >= 4) {
+    echo 'Il nostro sistema ha calcolato che hai: ' . $age . ' anni.';
+} else {
+    echo "troppo piccolo per scrivere a macchina";
 }

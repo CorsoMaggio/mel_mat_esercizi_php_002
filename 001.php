2257d735040839d9843cde55fe93d5f95ef945ad
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-10 10:07:49 +0200

    primo commit

---
+++ 001.php
@@ -0,0 +1,7 @@
+<?php
+const BUDGET= 1000;
+//
+$cifra= readline('Qual è la tuacifra?');
+if ($cifra>=BUDGET){
+    echo;
+}
---
+++ selezione.php
@@ -0,0 +1,12 @@
+<?php
+//condizione
+
+$age = 18;
+const MAX = 18;
+
+if ($age >= MAX) {
+    echo 'sono maggiorenne';
+} else {
+    echo 'sono minorenne';
+}
+//readline: input richiesto all'utente
